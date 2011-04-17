c9ac3986273fcfe322f6b5ed9c55d73dc7465939
Stephen Kraushaar <stephen@nova-initia.com>
2011-04-17 08:52:57 +0000

    Tools Models

--- models/ToolModel.js
+++ models/ToolModel.js
@@ -9,12 +9,73 @@ module.exports = function(mongoose) {
                 //End Shorthand Definitions
 
                 var TrapSchema = new Schema({
+                    '_id' : Number,
                     'user' : String,
                     'date' : Date,
                     'level' : Number
                 });
 
-                mongoose.model('Domain',DomainSchema);
+                mongoose.model('Trap',TrapSchema);
+                
+                var BarrelSchema = new Schema({
+                    '_id' : Number,
+                    'user' : String,
+                    'date' : Date,
+                    'level' : Number,
+                    'sg' : Number,
+                    'traps' : Number,
+                    'barrels' : Number,
+                    'spiders' : Number,
+                    'shields' : Number,
+                    'doorways' : Number,
+                    'signposts' : Number,
+                    'title' : String,
+                    'cmt' : String
+                });
+
+                mongoose.model('Barrel',BarrelSchema);
+
+                var SpiderSchema = new Schema({
+                    '_id' : Number,
+                    'user' : String,
+                    'date' : Date,
+                    'level' : Number
+                });
+
+                mongoose.model('Spider',SpiderSchema);
+
+                var DoorwaySchema = new Schema({
+                    '_id' : Number,
+                    'user' : String,
+                    'date' : Date,
+                    'level' : Number,
+                    'cmt' : String,
+                    'url' : String,
+                    'title' : String,
+                    'nsfw' : Boolean,
+                    'score' : Number,
+                    'votes' : Number,
+                    'count' : Number,
+                    'chain' : Number
+                    
+                });
+
+                mongoose.model('Doorway',DoorwaySchema);
+                
+                var SignpostSchema = new Schema({
+                    '_id' : Number,
+                    'user' : String,
+                    'date' : Date,
+                    'level' : Number,
+                    'cmt' : String,
+                    'url' : String,
+                    'title' : String,
+                    'nsfw' : Boolean,
+                    
+                });
+
+                mongoose.model('Signpost',SignpostSchema);
+                
 
         } //End If
 
