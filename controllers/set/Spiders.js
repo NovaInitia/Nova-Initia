51ac16db5ccedb7e1ad0a1a20dc5cdc2734a54a5
root <root@Development.(none)>
2011-08-03 07:22:46 +0000

    sets

---
+++ controllers/set/Spiders.js
@@ -0,0 +1,22 @@
+
+module.exports = function (App) {
+    return baseSet = require('../SendController')(App,
+        //Write Method
+        function(opts,dSend) {
+            
+            //Grab pageId
+            var pageId = parseInt(opts.to);
+            
+            //Arrange properties for write to Db
+            delete opts.to;
+            opts.date = new Date();
+            opts._id = opts.date.getTime();
+            
+            dSend("Pages",pageId,"spiders",opts);
+        },
+        //Validation Method
+        function(opts) {
+	        return typeof(opts.to) !== "undefined" && typeof(opts.from) !== "undefined";
+    	});
+};
+
